58e3738c5092f1f1fc09a89fa93c6d44dd780387
Nguyen Cong Duc <nguyencongduc13195@gmail.com>
2018-10-15 12:56:34 +0700

    add styles rental details, add pipe module, add cors

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,28 +1,36 @@
-import { RentalComponent } from './rental.component';
-import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
-import { RentalListComponent } from './rental-list/rental-list.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RentalService } from './shared/rental.service';
-import { RouterModule, Routes } from '@angular/router';
-import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import { RentalComponent } from "./rental.component";
+import { RentalListItemComponent } from "./rental-list-item/rental-list-item.component";
+import { RentalListComponent } from "./rental-list/rental-list.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RentalService } from "./shared/rental.service";
+import { RouterModule, Routes } from "@angular/router";
+import { RentalDetailComponent } from "./rental-detail/rental-detail.component";
+import { HttpClientModule } from "@angular/common/http";
+import { NgPipesModule } from "ngx-pipes";
 const rentalRoutes: Routes = [
-  {path: 'rentals', component: RentalComponent, children:[
-    {path: '', component: RentalListComponent},
-    {path: ':rentalID', component: RentalDetailComponent}
-  ]}
-]
+  {
+    path: "rentals",
+    component: RentalComponent,
+    children: [
+      { path: "", component: RentalListComponent },
+      { path: ":rentalID", component: RentalDetailComponent }
+    ]
+  }
+];
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(rentalRoutes)
+    RouterModule.forChild(rentalRoutes),
+    HttpClientModule,
+    NgPipesModule
   ],
   declarations: [
     RentalListItemComponent,
     RentalListComponent,
     RentalComponent,
-    RentalDetailComponent,
+    RentalDetailComponent
   ],
   providers: [RentalService]
 })
-export class RentalModule { }
+export class RentalModule {}
